181caf9ac065af496bb251864ccda9b96eeea167
rom341 <roman.murzik.antpnov@gmail.com>
2024-08-10 19:25:30 +0400

    proj created

---
+++ ASP-.NET-test-proj-1/ASP-.NET-test-proj-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_.NET_test_proj_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
